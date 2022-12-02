af3749c16ac92549c6b34399fed0294234fcfa9a
unrec <andrei.troitskiy@gmail.com>
2022-12-02 15:43:16 +0100

    initial schema added

---
+++ src/main/kotlin/com/unrec/imdb/rater/config/GraphQlConfig.kt
@@ -0,0 +1,19 @@
+package com.unrec.imdb.rater.config
+
+import graphql.scalars.ExtendedScalars
+import graphql.schema.idl.RuntimeWiring
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+import org.springframework.graphql.execution.RuntimeWiringConfigurer
+
+@Configuration
+class GraphQlConfig {
+
+    @Bean
+    fun runtimeWiringConfigurer(): RuntimeWiringConfigurer {
+        return RuntimeWiringConfigurer { wiringBuilder: RuntimeWiring.Builder ->
+            wiringBuilder.scalar(ExtendedScalars.Date)
+        }
+    }
+}
