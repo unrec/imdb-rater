cf9f7a0b8ef137214289f9c177a92415243e8e32
unrec <andrei.troitskiy@gmail.com>
2022-12-29 10:52:04 +0100

    "mostWatchedGenres" turned to single String

--- src/main/kotlin/com/unrec/imdb/rater/controller/StatisticsController.kt
+++ src/main/kotlin/com/unrec/imdb/rater/controller/StatisticsController.kt
@@ -3,6 +3,7 @@ package com.unrec.imdb.rater.controller
 import com.unrec.imdb.rater.model.RateStatistics
 import com.unrec.imdb.rater.service.RatingService
 import com.unrec.imdb.rater.utils.minutesToDurationString
+import com.unrec.imdb.rater.utils.toSingleString
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
@@ -18,4 +19,7 @@ class StatisticsController(private val ratingService: RatingService) {
 
     @SchemaMapping
     fun totalRuntime(rateStatistics: RateStatistics) = rateStatistics.totalRuntime.minutesToDurationString()
+
+    @SchemaMapping
+    fun mostWatchedGenres(rateStatistics: RateStatistics) = rateStatistics.mostWatchedGenres.toSingleString()
 }
